badc0e2c1bb2e30c3c35daa6fbec141d047aa2ed
Nithin.M <nithinvannan2004@gmail.com>
2025-02-10 10:12:41 +0530

    day 8

---
+++ dau 7/StringDemo3.java
@@ -0,0 +1,6 @@
+public class StringDemo3{
+    public static void main(String[] args) {
+        String s1=" Silence please dont taln";
+        System.out.println(s1.trim());
+    }
+}
---
+++ dau 7/compare1.java
@@ -0,0 +1,8 @@
+public class compare1 {
+    public static void main(String[] args) {
+        String s1="ab";
+        String s2="abc";
+        System.out.println(s1.compareTo(s2));
+    }
+        
+}
