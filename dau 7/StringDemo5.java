cb1a32a0b40fe12f82e8b11dd4642ae60e6688d1
Nithin.M <nithinvannan2004@gmail.com>
2025-02-10 10:50:21 +0530

    commit

---
+++ dau 7/StringDemo5.java
@@ -0,0 +1,9 @@
+public class StringDemo5 {
+    public static void main(String[] args) {
+        String s="hello world";
+        System.out.println(s.indexOf("w"));
+        System.out.println(s.indexOf("o",5));
+        System.out.println(s.lastIndexOf("o"));
+    }
+    
+}
